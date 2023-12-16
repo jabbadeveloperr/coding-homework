da23b4a395f70ee5452f78054906e192033b5122
Vitalii Lytvynov <jabbadeveloper@gmail.com>
2023-12-16 02:57:11 +0200

    delete comments

--- src/test/java/org/com/ResultCountTest.java
+++ src/test/java/org/com/ResultCountTest.java
@@ -15,14 +15,11 @@ public class ResultCountTest {
         IRandomGenerator generator = new RandomGen(numbers, probabilities);
         ResultCount resultCount = new ResultCount(generator);
 
-        // Generate result counts for 1000 iterations
         Map<Integer, Integer> resultCounts = resultCount.generateResultCounts(1000);
 
-        // Verify that the counts sum up to the total number of iterations
         int totalCount = resultCounts.values().stream().mapToInt(Integer::intValue).sum();
         assertEquals(1000, totalCount);
 
-        // Verify that each number is within the expected range
         for (int number : resultCounts.keySet()) {
             assertTrue(number >= -1 && number <= 3);
         }
@@ -33,7 +30,6 @@ public class ResultCountTest {
         float[] probabilities = {0.01f, 0.3f, 0.58f, 0.1f, 0.01f};
         IRandomGenerator generator = new RandomGen(numbers, probabilities);
         ResultCount resultCount = new ResultCount(generator);
-
         String printedResult = resultCount.printResultCounts(10);
         assertNotNull(printedResult);
     }
